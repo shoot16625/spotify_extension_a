c7f36f1755b712fd120413f9db7510a214ba9773
shoot16625 <shuto.uchida@gunosy.com>
2021-02-21 16:39:12 +0900

    first commit

---
+++ main/index.ts
@@ -0,0 +1,145 @@
+import express from 'express'
+import request from 'request'
+import cors from 'cors'
+import querystring from 'querystring'
+import cookieParser from 'cookie-parser'
+import rateLimit from 'express-rate-limit'
+import dotEnv from 'dotenv'
+dotEnv.config()
+
+const clientId = process.env.CLIENT_ID // Your client id
+const clientSecret = process.env.CLIENT_SECRET // Your secret
+const redirectUri = process.env.CALLBACK_URL // Your redirect uri
+const stateKey = 'spotify_auth_state'
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000 // limit each IP to 100 requests per windowMs
+})
+
+const generateRandomString = function (length: number): string {
+  let text = ''
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
+}
+
+const app: express.Express = express()
+
+app
+  .use(express.static(__dirname + '/public'))
+  .use(cors())
+  .use(cookieParser())
+  .use(limiter)
+
+app.get('/login', function (req: express.Request, res: express.Response) {
+  const state = generateRandomString(16)
+  res.cookie(stateKey, state)
+
+  const scope =
+    'user-read-private user-read-email user-library-read playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-follow-read'
+
+  res.redirect(
+    'https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirectUri,
+        state: state
+      })
+  )
+})
+
+app.get('/callback', function (req: express.Request, res: express.Response) {
+  const code = req.query.code || null
+  const state = req.query.state || null
+  const storedState = req.cookies ? req.cookies[stateKey] : null
+
+  if (state === null || state !== storedState) {
+    res.redirect(
+      '/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        })
+    )
+  } else {
+    res.clearCookie(stateKey)
+    const authOptions: request.Options = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        Authorization:
+          'Basic ' +
+          Buffer.from(clientId + ':' + clientSecret).toString('base64')
+      },
+      json: true
+    }
+
+    request.post(
+      authOptions,
+      function (error, response: request.Response, body) {
+        if (!error && response.statusCode === 200) {
+          const accessToken = body.access_token,
+            refreshToken = body.refresh_token
+          res.redirect(
+            '/#' +
+              querystring.stringify({
+                access_token: accessToken,
+                refresh_token: refreshToken
+              })
+          )
+        } else {
+          res.redirect(
+            '/#' +
+              querystring.stringify({
+                error: 'invalid_token'
+              })
+          )
+        }
+      }
+    )
+  }
+})
+
+app.get(
+  '/refresh_token',
+  function (req: express.Request, res: express.Response) {
+    const refreshToken = req.query.refresh_token
+    const authOptions: request.Options = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        Authorization:
+          'Basic ' +
+          Buffer.from(clientId + ':' + clientSecret).toString('base64')
+      },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+      },
+      json: true
+    }
+
+    request.post(
+      authOptions,
+      function (error, response: request.Response, body) {
+        if (!error && response.statusCode === 200) {
+          const accessToken = body.access_token
+          res.send({
+            access_token: accessToken
+          })
+        }
+      }
+    )
+  }
+)
+
+console.log('listening on 8888')
+app.listen(8888)
---
+++ main/public/statics/js/common.js
@@ -0,0 +1,347 @@
+function sleep(r) {
+  const sec = r * Math.random()
+  let objDef = new $.Deferred()
+  setTimeout(function () {
+    objDef.resolve(sec)
+  }, sec * 1000)
+  return objDef.promise()
+}
+
+function selectRandom(array, num) {
+  let newArray = []
+
+  while (newArray.length < num && array.length > 0) {
+    const rand = Math.floor(Math.random() * array.length)
+    newArray.push(array[rand])
+    array.splice(rand, 1)
+  }
+
+  return newArray
+}
+
+function getHashParams() {
+  let hashParams = {}
+  let e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1)
+  while ((e = r.exec(q))) {
+    hashParams[e[1]] = decodeURIComponent(e[2])
+  }
+  return hashParams
+}
+
+function sliceByNumber(array, number) {
+  const length = Math.ceil(array.length / number)
+  return new Array(length).fill().map((_, i) =>
+    array.slice(i * number, (i + 1) * number)
+  )
+}
+
+// Tokenをリフレッシュする
+function refleshToken(refreshToken) {
+  return new Promise(function (resolve) {
+    $.ajax({
+      url: '/refresh_token',
+      data: {
+        refresh_token: refreshToken
+      }
+    }).then((response) => resolve(response))
+  })
+}
+
+// ユーザの情報を取得する
+function getUserInfo(accessToken) {
+  return new Promise(function (resolve) {
+    $.ajax({
+      url: encodeURI('https://api.spotify.com/v1/me'),
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    }).then((response) => resolve(response))
+  })
+}
+
+// お気に入りに登録した曲を取得する
+function getSavedTracks(limit, offset, accessToken) {
+  return new Promise(function (resolve) {
+    sleep(0.5).done(function () {
+      $.ajax({
+        url: encodeURI('https://api.spotify.com/v1/me/tracks'),
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        },
+        data: {
+          limit: limit,
+          offset: offset
+        }
+      }).then((response) => resolve(response))
+    })
+  })
+}
+
+// フォローしているアーティストを取得する
+function getFollowingArtists(limit, accessToken) {
+  return new Promise(function (resolve) {
+    $.ajax({
+      url: encodeURI('https://api.spotify.com/v1/me/following'),
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      },
+      data: {
+        type: 'artist',
+        limit: limit
+      }
+    }).then((response) => resolve(response))
+  })
+}
+
+// アーティストのアルバムを取得する
+function getArtistAlbums(artist_id, limit, offset, accessToken) {
+  return new Promise(function (resolve) {
+    sleep(1).done(function () {
+      $.ajax({
+        url: encodeURI(
+          'https://api.spotify.com/v1/artists/' + artist_id + '/albums'
+        ),
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        },
+        data: {
+          limit: limit,
+          offset: offset
+        }
+      }).then(
+        (response) => resolve(response),
+        (error) => resolve({ items: [] })
+      )
+    })
+  })
+}
+
+// アルバムの収録曲を取得する
+function getAlbumTracks(album_id, limit, offset, albumNum, accessToken) {
+  return new Promise(function (resolve) {
+    sleep(albumNum / 80).done(function () {
+      $.ajax({
+        url: encodeURI(
+          'https://api.spotify.com/v1/albums/' + album_id + '/tracks'
+        ),
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        },
+        data: {
+          limit: limit,
+          offset: offset
+        }
+      }).then(
+        (response) => resolve(response),
+        (error) => resolve({ items: [] })
+      )
+    })
+  })
+}
+
+// 曲の特徴を取得する
+function getTracksFeature(trackIds, accessToken) {
+  const trackIdsStr = trackIds.join(',')
+  return new Promise(function (resolve) {
+    sleep(1).done(function () {
+      $.ajax({
+        url: encodeURI('https://api.spotify.com/v1/audio-features/'),
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        },
+        data: {
+          ids : trackIdsStr,
+        },
+        timeout: 2000
+      }).then(
+        (response) => resolve(response),
+        (error) => resolve({ audio_features: [] })
+      )
+    })
+  })
+}
+
+function dropUnusedInfo(items) {
+  let newItems = {}
+  Object.keys(items).forEach((id) => {
+    const value = items[id]
+    newItems[id] = {
+      id: value.id,
+      name: value.name,
+      uri: value.uri,
+      feature: value.feature,
+      artist: value.artists[0].name
+    }
+  })
+  return newItems
+}
+
+// キャッシュした曲を取得する
+function getCachedTracks(cacheKeys) {
+  let cacheItems = [],
+    items = {}
+  cacheKeys.forEach((cacheKey) => {
+    cacheItems.push(JSON.parse(localStorage.getItem(cacheKey)))
+  })
+  cacheItems.forEach((cacheItem) => {
+    if (cacheItem) {
+      items = Object.assign(items, cacheItem)
+    }
+  })
+  return items
+}
+
+// ユーザのプレイリストを取得する
+function getUserPlaylists(limit, offset, accessToken) {
+  return new Promise(function (resolve) {
+    $.ajax({
+      url: encodeURI('https://api.spotify.com/v1/me/playlists'),
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      },
+      type: 'GET',
+      data: {
+        limit: limit,
+        offset: offset
+      }
+    }).then((response) => resolve(response))
+  })
+}
+
+// プレイリストを作成する
+function createNewPlaylist(user_id, body, accessToken) {
+  return new Promise(function (resolve) {
+    $.ajax({
+      url: encodeURI(
+        'https://api.spotify.com/v1/users/' + user_id + '/playlists'
+      ),
+      type: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      },
+      dataType: 'json',
+      data: JSON.stringify(body)
+    }).then((response) => resolve(response))
+  })
+}
+
+// プレイリストに曲を追加する
+function addTracksToPlaylist(playlistId, body, accessToken) {
+  return new Promise(function (resolve) {
+    sleep(2).done(function () {
+      $.ajax({
+        url: encodeURI(
+          'https://api.spotify.com/v1/playlists/' + playlistId + '/tracks'
+        ),
+        type: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        },
+        dataType: 'json',
+        data: JSON.stringify(body)
+      }).then((response) => resolve(response))
+    })
+  })
+}
+
+// 曲を選ぶ1
+function extractTrackIdsOfFeature(
+  trackFeatures,
+  targetKeys,
+  thresholds,
+  validComp
+) {
+  let items = {}
+  Object.values(trackFeatures).forEach((track) => {
+    if (track.hasOwnProperty('feature')) {
+      if (validComp === '>=') {
+        if (track.feature[targetKeys[0]] >= thresholds[0]) {
+          items[track.id] = track
+        }
+      } else if (validComp === '<') {
+        if (track.feature[targetKeys[0]] < thresholds[0]) {
+          items[track.id] = track
+        }
+      }
+    }
+  })
+  return items
+}
+
+// 曲を選ぶ2
+function extractTrackIdsOf2Feature(
+  trackFeatures,
+  targetKeys,
+  thresholds,
+  validComp
+) {
+  let items = {}
+  Object.values(trackFeatures).forEach((track) => {
+    if (track.hasOwnProperty('feature')) {
+      if (validComp === '>=') {
+        if (
+          track.feature[targetKeys[0]] >= thresholds[0] &&
+          track.feature[targetKeys[1]] >= thresholds[1]
+        ) {
+          items[track.id] = track
+        }
+      } else if (validComp === '<') {
+        if (
+          track.feature[targetKeys[0]] < thresholds[0] ||
+          track.feature[targetKeys[1]] < thresholds[1]
+        ) {
+          items[track.id] = track
+        }
+      }
+    }
+  })
+  return items
+}
+
+// 曲をランダムに抽出する
+function extractRandomTracks(
+  items,
+  maxSize
+) {
+  const trackIds = Object.keys(items)
+  if (trackIds.length > maxSize) {
+    const selectedIds = selectRandom(trackIds, maxSize)
+    let restrictedItems = {}
+    selectedIds.forEach((id) => {
+      restrictedItems[id] = items[id]
+    })
+    return restrictedItems
+  }
+  return items
+}
+
+
+// css
+
+// range slider
+$(function () {
+  $('[type="range"]').on('change input', function () {
+    const elemId = $(this).attr('id')
+    const val = $('#' + elemId + '[type="range"]').val()
+    const rangePercent = 25 * val
+    if (rangePercent === 50) {
+      $('.' + elemId).html('<div class="slider-disabled-size">disabled</div><span></span>')
+      $('#' + elemId + '[type="range"], .' + elemId + ' span').css(
+        'filter',
+        'grayscale(70%)'
+      )
+    } else {
+      $('.' + elemId).html(((val - 2) / 2).toFixed(1) + '<span></span>')
+      $('#' + elemId + '[type="range"], .' + elemId + ' span').css(
+        'filter',
+        'hue-rotate(-' + rangePercent + 'deg)'
+      )
+    }
+    $('.' + elemId).css({
+      transform: 'translateX(-50%) scale(1.5)',
+      left: rangePercent + '%'
+    })
+  })
+})
